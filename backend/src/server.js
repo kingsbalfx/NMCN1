83e9d17cea26afb59f82c0888a080731bacc3113
kingsbalfx <shafiuabdullahi.sa3@gmail.com>
2026-01-24 15:01:39 +0100

    Re-enable questions routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,7 @@ require("dotenv").config();
 const express = require("express");
 
 const authRoutes = require("./modules/auth/auth.routes");
-// ⛔ TEMPORARILY DISABLE THESE
-// const questionRoutes = require("./modules/questions/questions.routes");
+const questionRoutes = require("./modules/questions/questions.routes");
 // const examRoutes = require("./modules/exams/exams.routes");
 // const adminRoutes = require("./modules/admin/admin.routes");
 // const paymentRoutes = require("./modules/payments/payments.routes");
@@ -13,6 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/questions", questionRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).send("Kingsbal API is running 🚀");
